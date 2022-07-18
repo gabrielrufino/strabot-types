5c13d9d720d7e040bf17f6fa14fa26b2afe7c507
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-18 19:22:24 -0300

    Initial types definition

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {Object} Command
+ * @property {Number} id
+ * @property {Object} attributes
+ * @property {String} attributes.Name
+ */
+
+/**
+ * @typedef {Object} Message
+ * @property {Number} id
+ * @property {Object} attributes
+ * @property {String} attributes.Text 
+ */
+
+export default {}
