49c5444561fbf852ed7e5bd93095dc36f8ae0b17
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-18 21:18:12 -0300

    More types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 /**
+ * @typedef {Object} Chat
+ * @property {Number} id
+ * @property {Object} attributes
+ * @property {String} attributes.Name
+ * @property {String} attributes.Platform
+ * @property {String} attributes.Type
+ * @property {String} attributes.Chat_ID
+ */
+
+/**
  * @typedef {Object} Command
  * @property {Number} id
  * @property {Object} attributes
@@ -6,10 +16,33 @@
  */
 
 /**
+ * @typedef {Object} DiscordConfig
+ * @property {Number} id
+ * @property {Object} attributes
+ */
+
+/**
+ * @typedef {Object} Listening
+ * @property {Number} id
+ * @property {Object} attributes
+ * @property {String} attributes.Label
+ * @property {String} attributes.Text
+ * @property {Object} [attributes.Messages]
+ * @property {Message[]} [attributes.Messages.data]
+ */
+
+/**
  * @typedef {Object} Message
  * @property {Number} id
  * @property {Object} attributes
+ * @property {String} attributes.Label
  * @property {String} attributes.Text
+ */
+
+/**
+ * @typedef {Object} SlackConfig
+ * @property {Number} id
+ * @property {Object} attributes
  */
 
 /**
